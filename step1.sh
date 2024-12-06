cd0621622b43e76cc65dbdde37d890f6991aa6fd
ramongallego <ramongallego@gmail.com>
2024-12-06 09:02:44 +0100

    two steps

---
+++ step1.sh
@@ -0,0 +1,13 @@
+#!usr/bin/bash
+
+conda activate cutadaptenv
+
+pcr_primers="/home/meg/rgallego/GTABE/pcr_primers.fasta"
+
+INPUT_files="/home/meg/rgallego/GTABE/fastq_pass"
+
+OUTPUT_folder="/home/meg/rgallego/GTABE/pipeline_output"
+
+
+which cutadapt
+
